23dbfcfcc13f8a303d1628107407ec452d4a8bff
BasuT-HTTS <141648088+BasuT-HTTS@users.noreply.github.com>
2024-12-03 11:18:27 +0530

    Update Live_PnL.py

--- Live_PnL.py
+++ Live_PnL.py
@@ -174,7 +174,7 @@ def main():
             if not intra_df.empty:
                 intra_df_sorted = intra_df.sort_values(by='PnL', ascending=False).drop(columns=['Category']).reset_index(drop=True)
                 intra_df_sorted['PnL'] = intra_df_sorted['PnL'].astype(int).apply(lambda x: f"{x:.0f}")
-                st.table(intra_df_sorted)  # This will display without index
+                st.table(intra_df_sorted, hide_index=True)
                 # st.dataframe(intra_df_sorted, hide_index=True)
                 # st.write(intra_df_sorted, hide_index=True)
             else:
